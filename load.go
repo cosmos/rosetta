299d74ed737241c36b922e8d288f4efaa9b1fbd9
bizk <santiago.yanzon1999@gmail.com>
2023-06-30 15:02:39 -0300

    added nolint on staticcheck on load.go

--- load.go
+++ load.go
@@ -1,4 +1,4 @@
-// nolint:staticcheckgit  //Since we are usinbg grpc_reflection_v1alpha which is deprecated, we excuse this rule
+// nolint:staticcheck  //Since we are usinbg grpc_reflection_v1alpha which is deprecated, we excuse this rule
 package rosetta
 
 import (
