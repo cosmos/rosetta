2c72368acbd2ef616a7479c68541cbcfc3f6a751
bizk <santiago.yanzon1999@gmail.com>
2023-09-11 15:01:38 -0300

    trying to solve untracked file issue

--- plugins/cosmos-hub/main.go
+++ plugins/cosmos-hub/main.go
@@ -3,9 +3,6 @@ package main
 import (
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
-	ibcclienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
-
-	ibcLightClient "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
@@ -20,8 +17,8 @@ func InitZone() {
 }
 
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
-	ibcclienttypes.RegisterInterfaces(registry)
-	ibcLightClient.RegisterInterfaces(registry)
+	// ibcclienttypes.RegisterInterfaces(registry)
+	// ibcLightClient.RegisterInterfaces(registry)
 	sdk.RegisterInterfaces(registry)
 	txtypes.RegisterInterfaces(registry)
 	cryptocodec.RegisterInterfaces(registry)
