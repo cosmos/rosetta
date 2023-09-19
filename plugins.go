68d54f85e89ed2d79747edd52f6228786b290335
bizk <santiago.yanzon1999@gmail.com>
2023-09-19 11:01:39 -0300

    small improvement on errors and fix on plugin deps

--- plugins.go
+++ plugins.go
@@ -1,11 +1,10 @@
 package rosetta
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"plugin"
-
-	crgerrs "github.com/cosmos/rosetta/lib/errors"
 
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 )
@@ -14,24 +13,24 @@ func LoadPlugin(ir codectypes.InterfaceRegistry, pluginLocation string) (err err
 	pluginPathMain := fmt.Sprintf("./plugins/%s/main.so", pluginLocation)
 
 	if _, err := os.Stat(pluginPathMain); os.IsExist(err) {
-		return crgerrs.WrapError(crgerrs.ErrPlugin, fmt.Sprintf("Plugin file '%s' does not exist %s", pluginPathMain, err.Error()))
+		return errors.New(fmt.Sprintf("Plugin file '%s' does not exist %s", pluginPathMain, err.Error()))
 	}
 
 	// load module
 	plug, err := plugin.Open(pluginPathMain)
 	if err != nil {
-		return crgerrs.WrapError(crgerrs.ErrPlugin, fmt.Sprintf("There was an error while opening plugin on %s - %s", pluginPathMain, err.Error()))
+		return errors.New(fmt.Sprintf("There was an error while opening plugin on %s - %s", pluginPathMain, err.Error()))
 	}
 
 	initZone, err := plug.Lookup("InitZone")
 	if err != nil {
-		return crgerrs.WrapError(crgerrs.ErrPlugin, fmt.Sprintf("There was an error while initializing the zone %s", err.Error()))
+		return errors.New(fmt.Sprintf("There was an error while initializing the zone %s", err.Error()))
 	}
 	initZone.(func())()
 
 	registerInterfaces, err := plug.Lookup("RegisterInterfaces")
 	if err != nil {
-		return crgerrs.WrapError(crgerrs.ErrPlugin, fmt.Sprintf("There was an error while registering interfaces %s", err.Error()))
+		return errors.New(fmt.Sprintf("There was an error while registering interfaces %s", err.Error()))
 	}
 
 	registerInterfaces.(func(codectypes.InterfaceRegistry))(ir)
