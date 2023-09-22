3430c752315a06a98dce2cd24e4c65021107e2f9
bizk <santiago.yanzon1999@gmail.com>
2023-09-22 18:39:06 -0300

    fixed script execution

--- scripts/rosetta-cli-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH=${PATH}:`go env GOPATH`/bin
-go install github.com/coinbase/rosetta-cli@v0.10.0
-rosetta-cli check:data  --in-memory-mode --configuration-file ./tests/config/rosetta-cli.json
