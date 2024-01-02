8bba81c3524f8647f920f9b52335cd0bd181ea95
bizk <santiago.yanzon1999@gmail.com>
2024-01-02 12:40:15 -0300

    added health check instead of sleep command

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,3 +1,5 @@
 /app/simd start &
-sleep 30
+until curl --output /dev/null --silent --head --fail http://localhost:26657/health; do
+   sleep 1
+done
 ./rosetta --blockchain "cosmos" --network "cosmos" --tendermint "tcp://localhost:26657" --addr "localhost:8080" --grpc "localhost:9090"
